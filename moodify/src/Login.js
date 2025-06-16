411839fac9d0a3afd81f2ca66333ede8a2f77076
WidowedStorm18 <82711432+GeekyNerd2005@users.noreply.github.com>
2025-06-16 21:38:58 +0530

    nxt

--- moodify/src/Login.js
+++ moodify/src/Login.js
@@ -1,7 +1,7 @@
 // src/Login.js
 export default function Login() {
   const clientId = "aed7c132277c4daba86f50210d48a5a5";
-  const redirectUri = "http://127.0.0.1:3000/";
+  const redirectUri = "https://moodifyinc.netlify.app/";
   const scopes = [
     "user-top-read",
     "playlist-modify-public",
